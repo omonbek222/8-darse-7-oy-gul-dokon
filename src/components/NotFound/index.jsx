53f349138e7a4037bf6423012d39a52fe57b8124
omonbek <jarimjonovomonbek@gmail.com>
2025-04-04 15:06:37 +0500

    omon11

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,26 +1,11 @@
-import { Result, Button } from "antd";
-import { useNavigate } from "react-router-dom";
+import React from 'react'
 
-const NotFound = () => {
-  const navigate = useNavigate();
+function index() {
   return (
-    <div className="flex justify-center items-center h-[100vh] w-full">
-      <Result
-        status="404"
-        title="404"
-        subTitle="Sorry, the page you visited does not exist."
-        extra={
-          <Button
-            onClick={() => navigate("/")}
-            type="default"
-            className="h-[40px] px-[10px]"
-          >
-            Back Home
-          </Button>
-        }
-      />
+    <div>
+      omonbek
     </div>
-  );
-};
+  )
+}
 
-export default NotFound;
+export default index
